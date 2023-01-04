a41a78bf9ea6b80a1afe239d67568a2cc6e46366
Rayyan Pratama <70933070+Reeman-30@users.noreply.github.com>
2023-01-05 01:16:20 +0700

    PBO Praktikum 8 MAVEN JAVA

---
+++ PBO-212310034-PRAKTIKUM-8/src/main/java/com/ibik/pbo/praktikum/MainApps.java
@@ -0,0 +1,12 @@
+package com.ibik.pbo.praktikum;
+
+import javax.swing.JOptionPane;
+
+public class MainApps {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Selamat Datang!");
+        new FormLogin().setVisible(true);
+    }
+    
+}
